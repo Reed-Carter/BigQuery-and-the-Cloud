7557ded564cd897c04b6b2cbe64da21b271ea201
Reed-Carter <reedandrew18@gmail.com>
2022-12-23 10:54:52 -0800

    create flower shop table in bigquery

---
+++ plants_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE plants.flower_shop (
+  plant_id integer NOT NULL,
+  species_name STRING DEFAULT NULL,
+  variety_name STRING DEFAULT NULL,
+  zone INTEGER DEFAULT NULL,
+  price FLOAT64 DEFAULT NULL,
+);
